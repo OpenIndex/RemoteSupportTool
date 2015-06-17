3d738026f0c5a20dff573bc06123f383e4111237
Andreas Rudolph <andy@openindex.de>
2015-06-17 18:29:00 +0200

    little improvements in build.sh

--- build.sh
+++ build.sh
@@ -23,18 +23,36 @@ mkdir -p "$TARGET"
 cd "$BASE_DIR"
 "$BUILD" --distpath="$TARGET" --workpath="target/build" "$SPEC"
 
-APP="$( ls -1 "$TARGET" | grep ".app" )"
-if [ ! -z "$APP" ]
+set +e
+
+MAC_APP="$( ls -1 "$TARGET" | grep ".app" )"
+if [ ! -z "$MAC_APP" ]
 then
-  echo "Post processing OS X application bundle '$APP'."
+  echo ""
+  echo "-------------------------"
+  echo ""
+  echo "Post processing OS X application bundle '$MAC_APP'."
 
   echo "Copy customized 'Info.plist'."
-  PLIST="$TARGET"/"$APP"/Contents/Info.plist
+  PLIST="$TARGET"/"$MAC_APP"/Contents/Info.plist
   cp -f "$BASE_DIR"/misc/darwin/Info.plist "$PLIST"
 
   echo "Create an archive."
   cd "$TARGET"
-  tar cfz "$APP.tar.gz" $APP
+  tar cfz "$MAC_APP.tar.gz" "$MAC_APP"
+fi
+
+LINUX_APP="$( ls -1 "$TARGET" | grep "_linux" )"
+if [ ! -z "$LINUX_APP" ]
+then
+  echo ""
+  echo "-------------------------"
+  echo ""
+  echo "Post processing Linux executable '$LINUX_APP'."
+
+  echo "Create an archive."
+  cd "$TARGET"
+  tar cfz "$LINUX_APP.tar.gz" "$LINUX_APP"
 fi
 
 echo ".... done."
